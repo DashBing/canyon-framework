437895fcda48140b8c6cfa12a3ac03351afbcae0
DashBing <103482780+DashBing@users.noreply.github.com>
2024-08-20 12:00:42 +0800

    fix bugs

--- src/memory.hpp
+++ src/memory.hpp
@@ -1,6 +1,5 @@
 #include "types.h"
 #include "constants.hpp"
-#include "constants.hpp"
 #include <vector>
 #include <stdexcept>
 namespace canyon{
@@ -14,15 +13,15 @@ namespace memory{
 class Memory{
     protected:
     std::vector<uint8_t *> memlist;
-    std::vector<wordlength_t> memindex;
+    std::vector<word_length_t> memindex;
 
     public:
-    void add_memory_block(wordlength_t size_byte){
+    void add_memory_block(word_length_t size_byte){
         memlist.push_back(new uint8_t[size_byte]);
         if(memindex.size() == 0)memindex.push_back(size_byte-1);
         else memindex.push_back(memindex.back()+size_byte);
     }
-    uint8_t & at(wordlength_t index){
+    uint8_t & at(word_length_t index){
         bool found = false;
         uint8_t *c;
         if(memindex.size()==0)memory::throw_out_of_range();
@@ -30,7 +29,7 @@ class Memory{
             found = true;
             c = memlist[0]+index;
         }
-        else if(memindex.size()>1)for(wordlength_t i=1; i<memindex.size(); i++){
+        else if(memindex.size()>1)for(word_length_t i=1; i<memindex.size(); i++){
             if(memindex[i-1]<index&&index<=memindex[i]){
                 found = true;
                 c = memlist[i]+index-memindex[i-1];
