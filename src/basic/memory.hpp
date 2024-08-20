30e78abc00b70e4c7d118e82d8c56b4bf4f3cdf0
DashBing <103482780+DashBing@users.noreply.github.com>
2024-08-20 17:44:23 +0800

    update memory

--- src/basic/memory.hpp
+++ src/basic/memory.hpp
@@ -19,12 +19,12 @@ class Memory{
     std::vector<word_length_t> memindex;
 
     public:
-    void add_memory_block(word_length_t size_byte){
+    void create_page(word_length_t size_byte){  // 创建内存页
         memlist.push_back(new uint8_t[size_byte]);
         if(memindex.size() == 0)memindex.push_back(size_byte-1);
         else memindex.push_back(memindex.back()+size_byte);
     }
-    uint8_t & at(word_length_t index){
+    uint8_t & at(word_length_t index){  // 索引位置
         bool found = false;
         uint8_t *c;
         if(memindex.size()==0)memory_priv::throw_out_of_range();
@@ -45,28 +45,45 @@ class Memory{
         }
         else memory_priv::throw_out_of_range();
     }
-    void clear(){
+    void clear(){  // 清空所有
         for(auto i : memlist){
             delete[] i;
         }
         memlist.clear();
         memindex.clear();
     }
-    word_length_t get_page_size(){
+    word_length_t get_page_size(){  // 取页数
         return(memlist.size());
     }
-    word_length_t size(){
+    word_length_t size(){  // 取总大小
         return(memindex.back());
     }
-    word_length_t check_page_size(word_length_t index){
+    word_length_t check_page_size(word_length_t index){  // 查看一个页的大小
         if(index>memindex.size()-1)return(0);
         else if(index==0)return(memindex[0]);
         else return(memindex[index]-memindex[index-1]);
     }
+    void remove_page(word_length_t index){  // 从表中移除页
+        if(index>memindex.size()-1)return;
+        word_length_t size = memindex.size()-1;
+        word_length_t l = memindex[index];
+
+        memlist.erase(memlist.begin()+index);
+        memindex.erase(memindex.begin()+index);
+        if(index==size)return;
+        for(int i = index; i<memindex.size(); i++){
+            memindex[i] -= l;
+        }
+    }
+    void delete_page(word_length_t index){
+        if(index>memindex.size()-1)return;
+        delete[] memlist[index];
+        remove_page(index);
+    }
 
     public:
     Memory(){}
-    Memory(word_length_t size_byte){add_memory_block(size_byte);}
+    Memory(word_length_t size_byte){create_page(size_byte);}
     ~Memory(){clear();}
 
     public:
