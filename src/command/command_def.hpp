09d417ccca1a2bdd7543ce551112b392c9f154cb
DashBing <103482780+DashBing@users.noreply.github.com>
2024-08-20 18:29:45 +0800

    add command_parser

--- src/command/command_def.hpp
+++ src/command/command_def.hpp
@@ -3,13 +3,13 @@
 #include "../basic/types.hpp"
 namespace canyon{
 enum command : uint16_t{  // 指令定义
-    sys_quit = 0x0101,
-    data_mov_i2r = 0x0200,
-    data_mov_r2m = 0x0201,
-    data_mov_m2r = 0x0202,
-    data_mov_i2m = 0x0203,
-    data_disk_d2m = 0x0204,
-    data_disk_m2d = 0x0205,
-    data_disk_i2d = 0x0206,
+    sys_quit = 0x0101,  // 退出
+    data_mov_i2r = 0x0200,  // 立即数存到寄存器
+    data_mov_r2m = 0x0201,  // 寄存器存到内存
+    data_mov_m2r = 0x0202,  // 内存存到寄存器
+    data_mov_i2m = 0x0203,  // 立即数存到内存
+    data_disk_d2m = 0x0204,  // 磁盘（文件）加载到内存
+    data_disk_m2d = 0x0205,  // 内存加载到磁盘
+    data_disk_i2d = 0x0206,  // 立即数存到磁盘
 };}
 #endif
--- src/command/parser.hpp
+++ src/command/parser.hpp
@@ -1,3 +1,10 @@
 #ifndef _CANYON_COMMAND_PARSER
 #define _CANYON_COMMAND_PARSER
+#include "../core.hpp"
+#include "command_def.hpp"
+namespace canyon{
+
+void command_parser(Core * core);
+
+}
 #endif
